343244fd9bd23a91f13e807772c5f2976af2a354
윤동현 <eatpaper1429@gmail.com>
2023-06-02 11:18:43 +0900

    mini-dooray-initial

---
+++ src/main/java/com/example/minidoorayproject/MiniDoorayProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.minidoorayproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDoorayProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniDoorayProjectApplication.class, args);
+	}
+
+}
